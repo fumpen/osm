57eb62d14c720d9e928055ddeabb699c1ebc87fa
Leed <frede_17@live.dk>
2016-02-23 13:17:41 +0100

    adding g2 to the repo

---
+++ g2/kudos/kudos/proc/process.h
@@ -0,0 +1,27 @@
+/*
+ * Process startup.
+ */
+
+#ifndef KUDOS_PROC_PROCESS
+#define KUDOS_PROC_PROCESS
+
+#include "lib/types.h"
+#include "vm/memory.h"
+
+#define PROCESS_PTABLE_FULL  -1
+#define PROCESS_ILLEGAL_JOIN -2
+
+#define PROCESS_MAX_FILELENGTH 256
+#define PROCESS_MAX_PROCESSES  128
+#define PROCESS_MAX_FILES      10
+
+typedef int process_id_t;
+
+typedef struct {
+  /* Remove this when you add other fields. */
+  int dummy;
+} process_control_block_t;
+
+void process_start(const char *executable, const char **argv);
+
+#endif
