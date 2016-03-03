3b53a88ddbb85972c951da114466334b7676a277
jeppeSS <jeppeskov92@gmail.com>
2016-03-03 15:52:00 +0100

    task 1

---
+++ g3/kudos/kudos/proc/usr_sem.h
@@ -0,0 +1,53 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  usr_sem.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  03-03-2016 12:24:28
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#ifndef KUDOS_PROC_SEM
+#define KUDOS_PROC_SEM
+
+#include "lib/types.h"
+#include "vm/memory.h"
+#include "kernel/spinlock.h"
+#include "kernel/semaphore.h"
+
+
+
+
+#define MAX_SEM 128
+#define NAME_LENGTH 20
+
+typedef struct {
+    char name[NAME_LENGTH];
+    int value;
+
+    spinlock_t slock;
+    semaphore_t* sem;
+
+} usr_sem_t;
+
+
+void usr_sem_init(void);
+
+usr_sem_t* usr_sem_open(const char* name, int value);
+
+int usr_sem_destroy(usr_sem_t* sem);
+
+int usr_sem_procure(usr_sem_t* sem);
+
+int usr_sem_vacate(usr_sem_t* sem);
+
+#endif
---
+++ g3/kudos/userland/semTest.c
@@ -0,0 +1,33 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  semTest.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  03-03-2016 15:17:58
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include "lib.h"
+
+#define BUFFER 60
+
+int main(void) {
+    
+
+    char prog[] = "[disk]hWorld.mips32";
+    int child;
+
+    printf("Starting program %s\n", prog);
+    
+
+    return 0;
+}
